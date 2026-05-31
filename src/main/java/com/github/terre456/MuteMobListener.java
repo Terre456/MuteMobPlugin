f02c3f52a47afb2f5e747352c134bbad4382bd34
Boris <sabredeboris@gmail.com>
2026-05-31 15:37:51 +0200

    commited now

---
+++ src/main/java/com/github/terre456/MuteMobListener.java
@@ -0,0 +1,9 @@
+package com.github.terre456;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class MuteMobListener implements Listener{
+  @EventHandler
+  public void onNameChanged()
+}
---
+++ src/main/java/com/github/terre456/MuteMobsPlugin.java
@@ -0,0 +1,16 @@
+package com.github.terre456;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class MuteMobsPlugin extends JavaPlugin{
+
+  @Override
+  public void onEnable(){
+    ;
+  }
+  
+}
